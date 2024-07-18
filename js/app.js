2590ff679e28f1c054623f6f9bd1db87ad211df6
Joabe Borges <joaberochaborges@hotmail.com>
2024-07-18 11:12:01 -0300

    Projeto de compra/saldo de ingressos - Primeiro commit

---
+++ js/app.js
@@ -0,0 +1,20 @@
+function comprar() {
+    
+    let tipoIngresso = document.getElementById('tipo-ingresso');
+    let QuantidadeIngresso = document.getElementById('qtd').value;    
+    let saldoIngressos;
+    let saldo;
+
+    //puxar o saldo de ingressos disponiveis para venda
+    saldoIngressos = document.getElementById(`qtd-${tipoIngresso.value}`);
+    
+    //Validar se o saldo disponivel é o suficiente para cobrir a demanda
+    saldo = parseInt(saldoIngressos.innerHTML) - QuantidadeIngresso
+
+    //Realizar ação de compra ou informar saldo indisponivel para compra
+    if (saldo < 0) {
+        alert(`Saldo insuficiente para compra de ingresso ${tipoIngresso.value}. Saldo disponivel: ${saldoIngressos.innerHTML}`);
+    } else {
+        saldoIngressos.innerHTML = saldo;        
+    }
+}
